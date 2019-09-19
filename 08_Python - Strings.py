172c24abf04b9c843077b3711e6001347d4ce9a5
alecktony <alecktony@yahoo.com>
2019-09-19 11:07:23 +0300

    Strings in Python

--- 08_Python - Strings.py
+++ 08_Python - Strings.py
@@ -146,36 +146,4 @@ print('The value of Integer1 is %3.2f' %Integer1)
 print("\nFormatting in 3.4f format: ") 
 print('The value of Integer1 is %3.4f' %Integer1) 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+print('Goodbye')
