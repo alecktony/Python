89e7be0d4a115b087de8832bba56d64de1efb2c7
alecktony <alecktony@yahoo.com>
2019-09-19 11:37:53 +0300

    Python Numbers

---
+++ 07_Python - Numbers.py
@@ -0,0 +1,32 @@
+a = 15
+
+a = 15.5
+
+print(int(a))
+
+print('\n')
+
+a = 16.25468845
+
+print(round(a,4))
+
+print('\n')
+
+from decimal import *
+
+a = Decimal(5.5)
+
+print(a)
+
+print('\n')
+
+import random
+
+print(random.random())
+
+print('\n')
+
+import random
+numbers = [1, 2, 3, 4, 5, 8, 12]
+
+print(random.choice(numbers))
